c976ca61bc4aa9823927f2de7cca9268e7e4823c
João Gabriel Winck Mendes <joaogwmendes@gmail.com>
2025-11-01 00:42:32 +0000

    feat: initialize React dashboard with Vite, Bootstrap, and ESLint
    - Added package.json with dependencies and scripts for development and build.
    - Included Vite configuration for React.
    - Created main application structure with App component rendering Login page.
    - Implemented Login page with Bootstrap components for form handling.
    - Added basic styling in App.css and index.css.
    - Included SVG assets for branding.
    - Created NavBar component for navigation.
    - Set up initial Card component structure.

---
+++ src/pages/Login.jsx
@@ -0,0 +1,44 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+
+export function Login() {
+    return (
+        <>
+            
+            <Container>
+                <Navbar className="mb-3" bg="dark" data-bs-theme="dark">
+                    <Container>
+                        <Navbar.Brand href="#">Minha App</Navbar.Brand>
+                    </Container>
+                </Navbar>
+            </Container>
+            <Container>
+            <Card border="secondary">
+                <Card.Body>
+                <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Senha</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Enviar
+                </Button>
+                </Form>
+                </Card.Body>
+                </Card>
+            </Container>
+            
+        </>
+    );
+}
